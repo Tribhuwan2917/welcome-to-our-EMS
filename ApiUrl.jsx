0934ba377bc44b8f7bfc49e1c2196c631a14d7d0
Tribhuwan2917 <Tribhuwan2917@users.noreply.github.com>
2024-03-07 07:07:45 +0000

    deploy: 219c2c95904f1f904da9e4120d8183ca188871ed

---
+++ ApiUrl.jsx
@@ -0,0 +1,79 @@
+// export const employeeManagement_base_URL="http://localhost:8081/employeeManagement/"
+export const employeeManagement_base_URL="http://54.236.134.70:8081/employeeManagement/"
+// //developement phase
+// for local production phase
+// export const employeeManagement_base_URL="http://localhost:8080/employee_management_backend/employeeManagement/"
+export const employeeManagement_attendance_punchIn="attendancePunchIn"
+export const employeeManagement_attendance_punchOut="attendancePunchOut"
+export const employeeManagement_attendance_getAllRecord="getAllAttendanceRecord"
+
+export const employeeManagement_leave_getLeaveRecord="getLeaveRecord"
+export const employeeManagement_leave_apply="leaveApply"
+
+export const employeeManagement_profile_getProfileByEmailId="getProfileByEmailId/"
+export const employeeManagement_message_sendEmail="sendEmail"
+export const employeeManagement_message_sendSMS="sendSMS"
+
+export const employeeManagement_registration_getAllRegistration_URL="getAllRegistration"
+export const employeeManagement_registration_getRegistration_URL="getRegistration/"
+export const employeeManagement_registration_postRegistration_URL="addRegistration"
+export const employeeManagement_registration_deleteRegistration_URL="deleteRegistration/"
+export const employeeManagement_registration_updateRegistration_URL="updateRegistration"
+// employeeManagement.registration.updateRegistration.URL=/updateRegistration
+export const employeeManagement_registration_forgotPasswordEmail_URL="forgotPasswordPostEmail"
+
+
+export const employeeManagement_employeeDetails_getAllEmployeeDetails_URL="getAllEmployeeDetails"
+export const employeeManagement_employeeDetails_getEmployeeDetails_URL="getEmployeeDetails/"
+export const employeeManagement_employeeDetails_postEmployeeDetails_URL="postEmployeeDetails"
+export const employeeManagement_employeeDetails_deleteEmployeeDetails_URL="deleteEmployeeDetails/"
+export const employeeManagement_employeeDetails_updateEmployeeDetails_URL="updateEmployeeDetails"
+
+
+export const employeeManagement_currentProject_getAllCurrentProject_URL="getAllCurrentProject"
+export const employeeManagement_currentProject_getCurrentProjectByEmployeeId_URL="getCurrentProjectByEmployeeId/"
+export const employeeManagement_currentProject_getCurrentProjectByCurrentProjectId_URL="getCurrentProjectByCurrentProjectId/"
+export const employeeManagement_currentProject_postCurrentProject_URL="addCurrentProject"
+export const employeeManagement_currentProject_deleteCurrentProject_URL="deleteCurrentProject/"
+export const employeeManagement_currentProject_updateCurrentProject_URL="updateCurrentProject"
+
+
+export const employeeManagement_priviousProject_getAllPriviousProject_URL="getAllPriviousProject"
+export const employeeManagement_priviousProject_getPriviousProjectByEmployeeId="getPriviousProjectByEmployeeId/"
+export const employeeManagement_priviousProject_getPriviousProjectByPriviousProjectId="getPriviousProjectByPriviousProjectId/"
+export const employeeManagement_priviousProject_postPriviousProject="postPriviousProject"
+export const employeeManagement_priviousProject_deletePriviousProject="deletePriviousProject/"
+export const employeeManagement_priviousProject_updatePriviousProject="updatePriviousProject"
+
+
+
+export const Instagram_logo_URL='https://res.cloudinary.com/dwnlcx35q/image/upload/v1705822564/dqminfdokxysgbtth1oa.avif'
+export const Linkdin_logo_URL='https://res.cloudinary.com/dwnlcx35q/image/upload/v1705822597/dylf1hzrmrhtm6meuixh.png'
+export const Github_logo_URL='https://res.cloudinary.com/dwnlcx35q/image/upload/v1705822546/gjwcnwrleybnch1disqh.png'
+export const Leetcode_logo_URL='https://res.cloudinary.com/dwnlcx35q/image/upload/v1705822582/ubziumycpmlmnj3t69g6.png'
+export const Twitter_logo_URL='https://res.cloudinary.com/dwnlcx35q/image/upload/v1705822684/whngqkbojskq1cetiowg.png'
+export const Employee_Image_Cloud_URL="https://api.cloudinary.com/v1_1/dwnlcx35q/upload";
+ 
+//https://res.cloudinary.com/dwnlcx35q/image/upload/v1709468977/Myphoto_pagaih.jpg
+export const Instagram_Account_URL='https://www.instagram.com/tribhuwan_1729/'
+export const Linkdin_Account_URL='https://www.linkedin.com/in/tribhuwan-pal-00553b215/'
+export const Github_Account_URL='https://github.com/Tribhuwan2917'
+export const Leetcode_Account_URL='https://leetcode.com/Tribhuwan_1729/'
+export const Twitter_Account_URL='https://twitter.com/pal_tribhu53871'
+ 
+export const Hr_Management_URL='/hrManagement'
+export const Manager_Management_URL='/managerManagent'
+export const Team_Lead_Management_URL='/teamLeadManagement'
+export const Senior_Manager_Management_URL='/seniorManagerManagement'
+export const Project_Management_URL='/projectManagement'
+
+export const Slider_Image1_URL='https://res.cloudinary.com/dwnlcx35q/image/upload/v1705822664/guufcaeljgkbhfneprlz.avif'
+export const Slider_Image2_URL='https://res.cloudinary.com/dwnlcx35q/image/upload/v1705822646/ih2mkuqdvx1hdc0db65a.avif'
+export const Slider_Image3_URL='https://res.cloudinary.com/dwnlcx35q/image/upload/v1705822630/hbhlcxys0uxm3c0czz1m.avif'
+export const Slider_Image4_URL='https://res.cloudinary.com/dwnlcx35q/image/upload/v1705822613/w1xsh9pbbreliys1f2uh.avif'
+
+
+
+
+
+
